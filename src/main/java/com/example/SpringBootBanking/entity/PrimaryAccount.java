7f94d4a9f02fbfcaa42efd347bf922ff36c09804
Shivakant6387 <shivakant5875@gmail.com>
2023-01-25 18:43:06 +0530

    primary Account update

--- src/main/java/com/example/SpringBootBanking/entity/PrimaryAccount.java
+++ src/main/java/com/example/SpringBootBanking/entity/PrimaryAccount.java
@@ -1,4 +1,27 @@
 package com.example.SpringBootBanking.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "primary_account")
 public class PrimaryAccount {
+    @Id
+    @Column(name = "account_id")
+    private String accountId;
+    @Column(name = "account_number")
+    private int accountNumber;
+    @Column(name = "account_balance")
+    private int accountBalance;
 }
--- src/main/java/com/example/SpringBootBanking/entity/UserAccount.java
+++ src/main/java/com/example/SpringBootBanking/entity/UserAccount.java
@@ -1,4 +1,17 @@
 package com.example.SpringBootBanking.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 public class UserAccount {
+
+
+
 }
